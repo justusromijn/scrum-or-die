89224a49fe8425a6669b1072fdd8ea6a05aad6c5
justusromijn <justus.romijn@gmail.com>
2018-03-28 11:17:22 +0200

    add webpack for bundling

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'public', 'assets', 'scripts')
+    },
+    devServer: {
+        publicPath: '/assets/scripts/',
+        contentBase: path.join(__dirname, 'public'),
+        port: 3000
     }
 };
